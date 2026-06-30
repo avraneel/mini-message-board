48c5f86ba25a520d10e1a4c44a81778493326e1b
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-01 00:29:49 +0530

    Add form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,4 +19,17 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini messages", messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = indexRouter;
