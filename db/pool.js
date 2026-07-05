25dc0734d477757c076926879034da47b137a5bb
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-05 08:06:18 +0530

    add passwd

--- db/pool.js
+++ db/pool.js
@@ -5,6 +5,7 @@ const { Pool } = require("pg");
 module.exports = new Pool({
   host: process.env.HOST,
   user: process.env.USER,
+  password: process.env.PASSWD,
   port: process.env.PORT,
   database: process.env.DB,
 });
